fa4c23c267cef66683cf9683a21825fe0828f581
the2nake <home.vn2007@gmail.com>
2022-11-04 14:14:15 +0800

    Added add row button and changing trait ability

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 function fieldEditCallback(e) {
     e = e || window.event;
+    /** @type HTMLElement */
+    e.target;
+    if (e.target.querySelector("div") != null && e.target.querySelector("div").classList.contains("temp-hover")) {
+        return;
+    }
 
     if (e.target.getElementsByTagName("div").length == 0) {
         var hoverInputContainer = document.createElement("div");
@@ -10,10 +15,11 @@ function fieldEditCallback(e) {
         hoverInputContainer.appendChild(hoverInput);
         hoverInputContainer.classList.add("hover-input-container");
         hoverInputContainer.classList.add("temp-hover");
-
+        
+        ///** @type DOMRect */
         var rect = e.target.getBoundingClientRect();
-        hoverInputContainer.style.top = (rect.top + rect.height / 2 + 6) + "px";
-        hoverInputContainer.style.left = rect.left + "px";
+        //hoverInputContainer.style.top = (e.target.scrollTop + rect.top + rect.height / 2 + 6) + "px";
+        //hoverInputContainer.style.left = rect.left + "px";
         hoverInput.style.width = rect.width + "px";
 
         e.target.appendChild(hoverInputContainer);
@@ -40,7 +46,14 @@ function linkEditCallback(e) {
         target = target.parentElement;
     }
 
+    if (target.querySelector("div") != null && target.querySelector("div").classList.contains("temp-hover")) {
+        return;
+    }
+
     var hoverInputContainer = document.createElement("div");
+    hoverInputContainer.classList.add("hover-input-container");
+    hoverInputContainer.classList.add("temp-hover");
+
     var hoverInputName = document.createElement("input");
     var hoverInputLink = document.createElement("input");
 
@@ -50,13 +63,11 @@ function linkEditCallback(e) {
     hoverInputLink.classList.add("temp-hover");
 
     var rect = target.getBoundingClientRect();
-    hoverInputContainer.style.top = (rect.top + rect.height / 2 + 6) + "px";
-    hoverInputContainer.style.left = rect.left + "px";
+    //hoverInputContainer.style.top = (target.scrollTop + rect.top + rect.height / 2 + 6) + "px";
+    //hoverInputContainer.style.left = rect.left + "px";
     hoverInputName.style.width = rect.width + "px";
     hoverInputLink.style.width = rect.width * 4 + "px";
 
-    hoverInputContainer.classList.add("hover-input-container");
-    hoverInputContainer.classList.add("temp-hover");
     hoverInputContainer.appendChild(hoverInputName);
     hoverInputContainer.appendChild(hoverInputLink);
 
@@ -79,25 +90,147 @@ function linkEditCallback(e) {
     hoverInputLink.onkeydown = linkEditSubmitCallback;
 }
 
-function raritySelectorCallback(e) {
+function raritySelectedCallback(e) {
+    e = e || window.event;
+    e.preventDefault();
+    /** @type HTMLElement */
+    let target = e.target;
 
+    while (!target.classList.contains("btn")) {
+        target = target.parentElement;
+    }
+
+    var clone = target.cloneNode(true);
+
+    while (!target.classList.contains("valid-target")) {
+        target = target.parentElement;
+    }
+
+    target.innerHTML = "";
+    target.appendChild(clone);
+}
+
+function raritySelectorCallback(e) {
+    e = e || window.event;
+    e.preventDefault();
+    /** @type HTMLElement */
+    let target = e.target;
+
+    while (target != null) {
+        if (target.classList.contains("valid-target")) {
+            break;
+        } else {
+            target = target.parentElement;
+        }
+    }
+
+    if (target == null) {
+        return;
+    }
+
+    if (target.querySelector("div") != null && target.querySelector("div").classList.contains("temp-hover")) {
+        // making sure there are no duplicates
+        return;
+    }
+
+    var hoverInputContainer = document.createElement("div");
+    hoverInputContainer.classList.add("hover-input-container");
+    hoverInputContainer.classList.add("temp-hover");
+    hoverInputContainer.style.height = "24px";
+    hoverInputContainer.style.paddingLeft = "8px";
+    hoverInputContainer.style.paddingRight = "8px";
+
+    hoverInputContainer.innerHTML = `
+    <a class="btn btn-sm bg-info tooltipster" data-toggle="tooltip" title="Common"><i
+    class="fas fa-star"></i></a>
+<a class="btn btn-sm bg-success tooltipster" data-toggle="tooltip" title="Uncommon"><i
+    class="fas fa-star"></i><i class="fas fa-star"></i></a>
+<a class="btn btn-sm bg-secondary tooltipster" data-toggle="tooltip" title="Rare"><i
+    class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></a>
+<a class="btn btn-sm bg-danger tooltipster" data-toggle="tooltip" title="Legendary"><i
+    class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i
+    class="fas fa-star"></i></a>
+<a class="btn btn-sm bg-warning tooltipster" data-toggle="tooltip" title="Mythical"><i
+    class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i
+    class="fas fa-star"></i><i class="fas fa-star"></i></a>
+<a class="btn btn-sm bg-light tooltipster" data-toggle="tooltip" title="Exclusive"><i
+    class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i
+    class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></a>
+    `;
+
+    var hoverChildren = hoverInputContainer.children;
+
+    for (let i = 0; i < hoverChildren.length; i++) {
+        hoverChildren[i].onclick = raritySelectedCallback;
+    }
+
+    target.appendChild(hoverInputContainer);
 }
 
 function initializeEditor(filestring) {
     var previewEl = document.getElementById("codePreview");
     previewEl.innerHTML = filestring;
-    let listingTableEl = previewEl.querySelectorAll("table.table tbody");
-    var listingRowEls = listingTableEl[listingTableEl.length - 1].getElementsByTagName("tr");
+    let listingTableEls = previewEl.querySelectorAll("table.table tbody");
+    var listingRowEls = listingTableEls[listingTableEls.length - 1].getElementsByTagName("tr");
     for (var i = 0; i < listingRowEls.length; i++) {
         var currentRowEls = listingRowEls[i].getElementsByTagName("td");
         for (var j = 0; j < 3; j++) {
             currentRowEls[j].onclick = fieldEditCallback;
+            currentRowEls[j].classList.add("valid-target");
         }
         currentRowEls[3].getElementsByTagName("a")[0].onclick = linkEditCallback;
+        currentRowEls[3].getElementsByTagName("a")[0].classList.add("valid-target");
         currentRowEls[4].onclick = raritySelectorCallback;
+        currentRowEls[4].classList.add("valid-target");
     }
 }
 
+function addRowCallback() {
+    var previewEl = document.getElementById("codePreview");
+
+    let listingTableEl = previewEl.querySelectorAll("table.table tbody")
+    listingTableEl = listingTableEl[listingTableEl.length - 1];
+
+    var blankTableRow = document.createElement("tr");
+    blankTableRow.innerHTML = `<td>00000</td>
+    <td style="width: 16.4035%;">@user</td>
+    <td style="width: 17.0176%;">@user</td>
+    <td><a href="LINK"><i class="fas fa-heart"></i>CHRNAME</a></td>
+    <td><a class="btn btn-sm bg-info tooltipster" data-toggle="tooltip" title="Common"><i
+                class="fas fa-star"></i></a></td>
+    <td>
+        <a data-toggle="collapse" href="#traits-00000">View All</a>
+        <div class="collapse" id="traits-00000">
+            <ul class="list-unstyled">
+                <li><a class="btn btn-sm bg-info tooltipster" data-toggle="tooltip" title="Common">TRAIT</a>
+                </li>
+            </ul>
+        </div>
+    </td>
+    <td>
+        <a data-toggle="collapse" href="#change-00000">View All</a>
+        <div class="collapse" id="change-00000"><br></div>
+    </td>
+    <td>
+        <a data-toggle="collapse" href="#trade-00000">View All</a>
+        <div class="collapse" id="trade-00000">
+            <ul class="list-unstyled">
+                <li>@user - type</li>
+            </ul>
+        </div>
+    </td>`;
+    listingTableEl.appendChild(blankTableRow);
+    
+    var currentRowEls = blankTableRow.getElementsByTagName("td");
+    for (var j = 0; j < 3; j++) {
+        currentRowEls[j].onclick = fieldEditCallback;
+        currentRowEls[j].classList.add("valid-target");
+    }
+    currentRowEls[3].getElementsByTagName("a")[0].onclick = linkEditCallback;
+    currentRowEls[3].getElementsByTagName("a")[0].classList.add("valid-target");
+    currentRowEls[4].onclick = raritySelectorCallback;
+    currentRowEls[4].classList.add("valid-target");
+}
 
 /** @type HTMLInputElement */
 var UploadFileEl = document.getElementById("codeInput");
